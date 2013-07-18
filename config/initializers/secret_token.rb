8d3bb9b5b24f936e89eafbe4070ef1d07a55e50a
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-18 17:14:57 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindMeJobsBetaSite::Application.config.secret_token = 'a0f7303f8d260f377f2f971dd4149ae9b8a8cc0726d4e7403fa142c5b7f712adee0dee263a8d63e6104a0ccbd3f75d9d85a301b6fb3490b10b8047442550b2ea'
