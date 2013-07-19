ac2bbcd981b3666274aa7ef05e9749a50565944b
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-19 14:28:18 -0500

    Added account to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
+  attr_accessible :email, :account
   VALID_EMAIL_REGEX = /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   	uniqueness: true
+  validate :check_account
+
+  private
+
+  	def check_account
+  		self.account = "Job Seeker" unless self.account == "Employer"
+  	end
 
 end
