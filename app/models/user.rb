3d57046c56a27328b474fcda81ef044695002532
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-19 12:36:52 -0500

    Refixed red input regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email
-  VALID_EMAIL_REGEX = /^\S+@\S+\.\S+$/i
+  VALID_EMAIL_REGEX = /^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   	uniqueness: true
 
