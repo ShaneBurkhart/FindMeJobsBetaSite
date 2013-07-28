ed8fcf2a0a0fb8b13ce87fd8f2d0b2616983f51f
ShaneBurkhart <shaneburkhart@gmail.com>
2013-07-28 13:35:08 -0500

    Added email to send to admin

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,17 @@ class UserMailer < ActionMailer::Base
   default from: "feedback@underjobs.me"
 
   def welcome_email(user)
+    @count = User.count
+    @employers_count = User.where(account: "Employer").count
+    @job_seeker_count = User.where(account: "Job Seeker").count
     @user = user
     if @user.employer?
       template = "welcome_email_employer"
     else
       template = "welcome_email_job_seeker"
     end
+    mail(to: "jobapp417@gmail.com", subject: "#{@user.email} - Another User Signed Up",
+        template_name: "user_signed_up").deliver
     mail(to: @user.email, subject: 'Welcome to FindMe.Jobs Beta',
         template_name: template)
   end
